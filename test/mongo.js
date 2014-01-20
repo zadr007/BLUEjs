100bb31ecea59b5f55bf1071f547c6656ebc45fc
Tomas Korcak <korczis@gmail.com>
2014-01-20 21:52:01 +0100

    Passing tests ...

--- test/mongo.js
+++ test/mongo.js
@@ -22,7 +22,7 @@
         beforeEach(function () {
             var config = utils.loadConfig(path.join(__dirname, "../config.js"), "test");
 
-            var Mongo = require('../modules/mongo/mongo');
+            var Mongo = require('../modules/mongo');
             mongo = new Mongo(config);
         });
 
