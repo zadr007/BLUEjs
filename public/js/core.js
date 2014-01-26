6d046830901c97f79092e83e684d74f842816043
Tomas Korcak <korczis@gmail.com>
2014-01-26 13:45:32 +0100

    Remove spam

--- public/js/core.js
+++ public/js/core.js
@@ -22,15 +22,6 @@
 
 (function (global) {
     define(["./config", "jquery", "bootstrap", "handlebars", "ember", "socketio", "exports"], function (config, $, bootstrap, handlebars, Ember, io, exports) {
-        var generateUUID = function () {
-            var d = new Date().getTime();
-            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = (d + Math.random() * 16) % 16 | 0;
-                d = Math.floor(d / 16);
-                return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-            });
-            return uuid;
-        };
 
         // See https://github.com/zeflasher/ember-require-js/blob/master/app/js/main.js
         require
